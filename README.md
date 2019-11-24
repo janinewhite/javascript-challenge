c4eb603ee1c1bf2db1e585645e1f267535e923c6
Janine White <janine.white@gmail.com>
2019-11-24 12:47:23 -0700

    Filter working

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # javascript-challenge
+
+Created a web page using HTML, CSS, Bootstrap, Javascript and D3 to filter a list of UFO sightings by date
+
+https://janinewhite.github.io/javascript-challenge/
+
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,31 @@
-var dateFilterButton = document.getElementById("DateFilter")
-var dateInput = document.getElementById("DateInput")
-console.log("Input default: "+document.getElementById("DateInput").value)
+// Get data from data.js
+var tableData = data;
+var firstRow = tableData[0]
+
+// Define table elements
 var table = document.querySelector("table");
 var thead = document.querySelector("thead");
 var theadRow = thead.querySelector("tr");
 var tbody = document.querySelector("tbody");
-dateFilterButton.addEventListener("click", buttonAlert);
-
-function deleteRows(table) {
-    var rowCount = table.rows.length;
-    var rowsDeleted = 0;
-    for (var i = 0; i < rowCount; i++) {
-        table.deleteRow[i];
-        rowsDeleted += 1;
-    }
-    return rowsDeleted;
-}
-
-function buttonAlert() {
-    alert(deleteRows(table));
-}
-
-// from data.js
-var tableData = data;
-var firstRow = tableData[0]
 
 // Capitalize string
 function capitalize(text) {
     return text.slice(0,1).toUpperCase()+text.slice(1)
 }
 
+// Capitalize all words in a string
+function titleCase(text) {
+    return text.toLowerCase().split(' ').map(x => capitalize(x)).join(' ');
+}
+
 // Create table header
 function generateTableHead(row,firstRow) {
-    //let thead = table.createTHead();
+    console.log("Loading Table Header");
+    // Use keys as table header
     keyNames = Object.keys(firstRow)
     for (keyName of keyNames) {
-        keyName = keyName[0].toUpperCase()+keyName.slice(1)
+        keyName = capitalize(keyName);
+        // Fix header text
         if (keyName == "Datetime") {
             keyName = "Date"
         } else {
@@ -43,8 +33,9 @@ function generateTableHead(row,firstRow) {
                 keyName = "Duration"
             }
 
-        }       
-        let th = document.createElement("th");
+        }
+        // Add header cell
+        var th = document.createElement("th");
         let text = document.createTextNode(keyName);
         th.appendChild(text);
         row.appendChild(th);
@@ -52,14 +43,23 @@ function generateTableHead(row,firstRow) {
 }
 
 // Create table body
-function generateTable(body, data, dateFilter) {
+function generateTableBody(body, data, dateFilter) {
+    console.log("Loading Table Body, Filter: "+dateFilter);
     for (element of data) {
         if ((dateFilter == "") || (dateFilter == element.datetime) ) {
             let row = body.insertRow();
             for (key in element) {
                 let cell = row.insertCell();
-                let text = document.createTextNode(element[key]);
-                cell.appendChild(text);
+                let celltext = element[key]
+                if ((key == "state") || (key == "country")) {
+                    celltext = celltext.toUpperCase();
+                } else {
+                    if (key == "city") {
+                        celltext = titleCase(celltext);
+                    }
+                }
+                let textnode = document.createTextNode(celltext);
+                cell.appendChild(textnode);
             }
         }
     }
@@ -73,6 +73,26 @@ function replaceTableBody(body, tableData) {
     generateTable(tbody, tableData, filter);
 }
 
-generateTable(tbody, tableData, "");
+// Add button click event
+var button = d3.select("#DateFilter");
+button.on("click", redoTableRows);
+
+// Add input change event
+var inputField = d3.select("#DateInput");
+var newText = "";
+inputField.on("change", function() {
+  newText = d3.event.target.value;
+  console.log("Input changed: "+newText);
+});
+
+// Remove table body rows, then add filtered rows
+function redoTableRows() {
+    let d3tbody = d3.select("tbody");
+    d3tbody.html("");
+    generateTableBody(tbody, data, newText)
+}
+
+// Create Curated Sightings Table
+generateTableBody(tbody, tableData, "");
 generateTableHead(theadRow, firstRow);
 
